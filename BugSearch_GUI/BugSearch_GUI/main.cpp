9f9a687d902c6d63625f6617b1209603caa92e95
Simic Nenad <simicdnenad@gmail.com>
2024-10-10 23:07:45 +0200

    Added Icon to the App.

--- BugSearch_GUI/BugSearch_GUI/main.cpp
+++ BugSearch_GUI/BugSearch_GUI/main.cpp
@@ -3,8 +3,35 @@
 
 #include "cmainwidget.h"
 
+#include <linux/limits.h>
+#include <memory>
+
+/**
+ * @brief Retreives path of the logo icon
+ *
+ * Takes absolute path of apps binary file, parse and modify it to the path of logo icon.
+ *
+ * @return shared_ptr<char[]> keeps the path to the icon's .png file
+ */
+std::shared_ptr<char[]> icoPath(const char* strBinPath) {
+    std::shared_ptr<char[]> pCwd (new char[PATH_MAX]);
+
+    std::string strPath(strBinPath);
+    // go back one directory and add icon's path
+    std::size_t found = strPath.find_last_of("/");
+    strPath = strPath.substr(0,found);
+    found = strPath.find_last_of("/");
+    strPath = strPath.substr(0,found);
+    strPath += "/BugSearch_GUI/download.png";
+    strcpy(pCwd.get(), strPath.c_str());
+
+    return pCwd;
+}
+
+
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(icoPath(argv[0]).get()));
 
     CMainWidget window;
 #ifdef NOTDEF
